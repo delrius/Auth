36f2b14ec42c819233227706b47b5467e688c6fc
delrius <delrius.ua@gmail.com>
2014-01-26 17:27:15 +0200

    basic functionality

--- src/main/java/ua/kiev/naukma/auth/client/LoginPanel.java
+++ src/main/java/ua/kiev/naukma/auth/client/LoginPanel.java
@@ -1,84 +1,36 @@
 package ua.kiev.naukma.auth.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.editor.client.Editor;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public class LoginPanel extends Composite implements IsWidget {
+public class LoginPanel extends Composite {
 
-    public LoginPanel() {
-    }
+    @UiField
+    LoginEditor loginEditor;
 
     @Override
     public Widget asWidget() {
-        LoginEditor editor = new LoginEditor();
-        driver.initialize(editor);
+        driver.initialize(loginEditor);
         driver.edit(new LoginModel());
-        return editor.asWidget();
+        return this;
     }
 
     interface Driver extends SimpleBeanEditorDriver<LoginModel, LoginEditor> {
     }
 
-    // Create the Driver
     Driver driver = GWT.create(Driver.class);
-}
 
-class LoginEditor extends Composite implements Editor<LoginModel> {
-
-    public interface Binder1 extends UiBinder<Widget, LoginEditor> {
+    interface LoginPanelUiBinder extends UiBinder<HorizontalPanel, LoginPanel> {
     }
 
-    private static Binder1 uiBinder = GWT.create(Binder1.class);
-    private static MyBundle bundle = MyBundle.INSTANCE;
-    private static Messages messages = GWT.create(Messages.class);
+    private static LoginPanelUiBinder ourUiBinder = GWT.create(LoginPanelUiBinder.class);
 
-    @UiField
-    @Ignore
-    HorizontalPanel horizontalPanel;
-
-    @UiField(provided = true)
-    @Ignore
-    FlexTable flexTable;
-
-    @Ignore
-    Label loginLabel;
-    @Ignore
-    Label passwordLabel;
-    @Ignore
-    Image loginError;
-    @Ignore
-    Image passwordError;
-
-    TextBox login;
-    PasswordTextBox password;
-
-    public LoginEditor() {
-        flexTable = new FlexTable();
-        login = new TextBox();
-        password = new PasswordTextBox();
-        initWidget(uiBinder.createAndBindUi(this));
-    }
-
-    public Widget asWidget() {
-        loginLabel = new Label(messages.loginLabel());
-        passwordLabel = new Label(messages.passwordLabel());
-
-        loginError = new Image(bundle.errorIcon());
-        loginError.setTitle("Password is required");
-
-        passwordError = new Image(bundle.errorIcon());
-        passwordError.setTitle("Login is required");
-
-        flexTable.setWidget(0, 0, loginLabel);
-        flexTable.setWidget(0, 1, login = new TextBox());
-        flexTable.setWidget(1, 0, passwordLabel);
-        flexTable.setWidget(1, 1, password = new PasswordTextBox());
-        flexTable.setWidget(1, 2, passwordError);
-        flexTable.setWidget(0, 2, loginError);
-        return this;
+    public LoginPanel() {
+        initWidget(ourUiBinder.createAndBindUi(this));
     }
 }
